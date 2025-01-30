f538e87079346a510558f0829cb9f1f89a7dca47
Vrusharth <vrusharth2003@gmail.com>
2025-01-30 10:14:51 +0530

    pattern

---
+++ Patterns/lvl1.java
@@ -0,0 +1,13 @@
+public class lvl1{
+    static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
